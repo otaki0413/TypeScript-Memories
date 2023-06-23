f8302b8a24042d4ad3fbcf6f91949e88e5129348
otaki0413 <takida0413@gmail.com>
2023-06-23 22:42:22 +0900

    p36 数値リテラルは式である

---
+++ 2.3/2.ts
@@ -0,0 +1,11 @@
+// 数値リテラルとは、数値という結果を生み出すための式
+const binary = 0b1010; // 2進数リテラル
+const octal = 0o755; // 8進数リテラル
+const hexadecimal = 0xff; // 16進数リテラル
+
+console.log(binary, octal, hexadecimal); // 10 493 255
+
+const big = 1e8; // 100000000
+const small = 4e-5; // 0.00004
+
+const million = 1_000_000; // 1000000
