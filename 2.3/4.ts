ae8d7195e188c697b713114b069f6f517b1653db
otaki0413 <takida0413@gmail.com>
2023-06-23 22:58:02 +0900

    p39 文字列には文字列リテラルとテンプレートリテラルがある

---
+++ 2.3/4.ts
@@ -0,0 +1,7 @@
+const str1: string = "hello";
+const str2: string = "world";
+console.log(`${str1}, ${str2}`);
+
+// テンプレートリテラルは ${式} という構文で式の値を文字列の構成に使用可能
+
+console.log(`123 + 456 =  ${123 + 456}`); // 123 + 456 =  579
