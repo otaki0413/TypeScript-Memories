669afa4180f781f9ee2f1b5bedef60969643ff55
otaki0413 <takida0413@gmail.com>
2023-06-25 03:13:52 +0900

    p41 真偽値とnull, undefinedはプリミティブである

---
+++ 2.3/5.ts
@@ -0,0 +1,11 @@
+// 真偽値リテラル
+const no: boolean = false;
+const yes: boolean = true;
+
+// nullとundefinedはプリミティブ型
+const va1l = null;
+const val2 = undefined;
+
+// ↑に対応する型
+const n: null = null;
+const u: undefined = undefined;
