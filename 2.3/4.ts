d5779208dfdf7faa5acd0905f0573a32ff34c0ab
otaki0413 <takida0413@gmail.com>
2023-06-25 03:03:01 +0900

    p40 エスケープシーケンスは 「\」 で始まる

--- 2.3/4.ts
+++ 2.3/4.ts
@@ -5,3 +5,12 @@ console.log(`${str1}, ${str2}`);
 // テンプレートリテラルは ${式} という構文で式の値を文字列の構成に使用可能
 
 console.log(`123 + 456 =  ${123 + 456}`); // 123 + 456 =  579
+
+// 改行を表すエスケープシーケンス \n
+console.log("Hello \nWorld");
+
+// タブ文字を表すエスケープシーケンス \t
+console.log("Hello \tWorld");
+
+// バックスラッシュを文字列に含める書き方 \\
+console.log("Hello \\World");
