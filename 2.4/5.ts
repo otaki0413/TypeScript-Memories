0add47ecbeca4f73955bb70a3a0af546874f48ea
otaki0413 <takida0413@gmail.com>
2023-07-15 21:42:45 +0900

    p57~61 条件演算子と代入演算子を学んだ

---
+++ 2.4/5.ts
@@ -0,0 +1,11 @@
+// 代入演算子
+let foo: string = "文字列";
+foo = 123;
+
+let num = 0;
+num += 100; // 100
+num *= 4; // 400
+num -= 200; // 200
+num /= 2; // 100
+num **= 0.5; // 10
+console.log(num); // 10
